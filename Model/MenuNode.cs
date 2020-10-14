c99419aa952643a89f2a35acb4b054f6fb89ae93
Christian Matyas <christian.matyas@lifbi.de>
2020-10-14 21:38:42 +0200

    init

---
+++ Model/MenuNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NepsSidenav.Model
+{
+    public class MenuNode
+    {
+        public List<MenuNode> Children { get; set; }
+        public bool Selected { get; set; }
+        public bool Last { get; set; }
+        public bool First { get; set; }
+        public bool Enabled { get { return true; } }
+        public bool Breadcrumb { get; set; }
+        public int Depth { get; set; }
+        public int Id { get; set; }
+        public string Url{ get {return "#";} }
+        public string Text { get { return "Node "  + Depth + "-" + Id + " " + " "+ (Selected?" (Selected)": "") + (Breadcrumb ? " (Bread)" : ""); } }
+    }
+}
